e142d0528c6f89861bba50392cb60872c40dc27f
robbedec <robbe.decorte@student.hogent.be>
2019-11-13 11:55:38 +0100

    test: main menu ui tests

--- app/src/androidTest/java/com/example/projecten3android/MainMenuDefault.kt
+++ app/src/androidTest/java/com/example/projecten3android/MainMenuDefault.kt
@@ -1,12 +1,21 @@
 package com.example.projecten3android
 
+import android.os.SystemClock
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import com.klimaatmobiel.ui.MainActivity
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,11 +23,36 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class MainMenuDefault {
+
+    @get:Rule
+    var activityRule: ActivityTestRule<MainActivity>
+            = ActivityTestRule(MainActivity::class.java)
+
+    @Before
+    fun setUp() {
+        onView(withId(R.id.project_code_text)).perform(clearText())
+    }
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.projecten3android", appContext.packageName)
+    fun testLoginButtonClickable() {
+        onView(withId(R.id.webshop_button)).check(matches(isClickable()))
+    }
+
+    @Test
+    fun testTextFieldHint() {
+        onView(withId(R.id.project_code_text)).check(matches(withHint(R.string.edit_text_hint)))
+    }
+
+    @Test
+    fun testCheckIfRedirectedToWebshopCorrectProjectCode() {
+        onView(withId(R.id.project_code_text)).perform(typeText("212345"))
+        onView(withId(R.id.webshop_button)).perform(click())
+
+        // Wait while the shop is loading
+        SystemClock.sleep(3000)
+
+        onView(withId(R.id.products_list)).check(matches(isDisplayed()))
+        onView(withId(R.id.constraintLayout_order)).check(matches(isDisplayed()))
     }
 }
